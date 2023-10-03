5f4bd6652a35c609858e56e84924103e27c28c4b
jas0307 <mtx86@hotmail.com>
2023-10-02 23:16:52 -0300

    V1.0

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ this.lugar=lugar
 this.observacion=observacion
 
 }
-
+//Inventario inicial
 let bien1 = new Inmueble (0,"pc","acer",5,350000,"sala de computacion","sin observación")
 let bien2 = new Inmueble (1,"pc","lenovo",4,450000,"sala de profesores","pc 2 sin pantalla")
 let bien3 = new Inmueble (2,"escritorio","ikea",5,100000,"sala de computacion","sin observación")
@@ -24,6 +24,10 @@ let bien6 = new Inmueble (5,"libro matematicas","santillana",40,35000,"bibliotec
 let bien7 = new Inmueble (6,"libro ciencias","editorial planeta",39,32000,"biblioteca","1 libro roto")
 
 let inventario = [bien1,bien2,bien3,bien4,bien5,bien6,bien7]
+
+console.log("Comandos consola: function agregarProducto(),function nuevoStock(),function nuevoPrecio(),function nuevaObservacion(),function filtrarInventario()")
+console.log("Inventario Actual")
+console.table(inventario)
 
 function filtrarInventario(){ //funcion para filtrar
 let palabraClave = prompt("ingresa el inmueble que deseas buscar").trim().toUpperCase()
@@ -36,13 +40,15 @@ alert("no se encontro ninguna coincidencia con "+ palabraClave)}
 }
 
 function agregarProducto(){//funcion para agregar nuevo producto
-let item = parseInt(prompt("ingresa cantidad"))
+
+let ultimoItem = inventario[inventario.length - 1].item;
+let item = ultimoItem +1 //aumenta al siguiente item creado
 let nombre = prompt ("ingresa nombre del inmueble").trim()
 let marca = prompt ("ingresa la marca del inmueble").trim()
 let stock = parseInt(prompt("ingresa cantidad"))
 let precio = parseInt(prompt("ingresa precio del inmueble"))
 let lugar = prompt ("ingresa en que lugar se encuentra").trim()
-let observacion = prompt ("ingresa en que lugar se encuentra").trim()
+let observacion = prompt ("ingresa observacion").trim()
 
 if(isNaN(precio)|| isNaN(stock)|| isNaN(item) || nombre ==="" || marca ===""|| lugar ===""|| observacion ===""){
 alert("porfavor ingresa valores validos")
@@ -61,12 +67,19 @@ function nuevoStock(){//funcion para agregar nuevo stock
     if(isNaN(valor1)|| isNaN(valor2)|| valor1 <= 0 ){
         alert("porfavor ingresa valores validos")
         return}
+    
     let stock2 = inventario[valor1].stock
     let suma = stock2 + valor2
-    inventario[valor1].stock = suma
-    console.log(suma)
-    console.log("Nuevo Stock")
-    console.table(inventario)
+    if(suma > 0 ){        
+        inventario[valor1].stock = suma
+        console.log(suma)
+        console.log("Nuevo Stock")
+        console.table(inventario)}
+     else{
+            alert("No queda stock")
+        }
+    
+    
 }
 
 function nuevoPrecio(){//funcion para agregar nuevo precio a item anterior
@@ -74,7 +87,7 @@ function nuevoPrecio(){//funcion para agregar nuevo precio a item anterior
     console.table(inventario)
     let valor1 = parseInt(prompt("ingresa item "))
     let valor2 = parseInt(prompt("ingresa cantidad "))
-    if(isNaN(valor1)|| isNaN(valor2)|| valor1 <= 0 ){
+    if(isNaN(valor1)|| isNaN(valor2)|| valor1 <= 0 || valor2 < 0){
         alert("porfavor ingresa valores validos")
         return}
 
